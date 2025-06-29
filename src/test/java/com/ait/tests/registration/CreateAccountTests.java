ba71cecbd9f80f2d6fb9afeefadfabc436fb1195
IonCraciun19 <icraciu21@gmail.com>
2025-06-29 17:43:23 +0200

    2

--- src/test/java/com/ait/tests/registration/CreateAccountTests.java
+++ src/test/java/com/ait/tests/registration/CreateAccountTests.java
@@ -1,6 +1,7 @@
 package com.ait.tests.registration;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CreateAccountTests extends TestBase{
@@ -8,12 +9,13 @@ public class CreateAccountTests extends TestBase{
     @Test
     public void createAccount(){
         click(By.cssSelector("[href='/register']"));
-        type(By.name("FirstName"),"Greck");
-        type(By.name("LastName"),"Ivan");
-        type(By.name("Email"),"igreck2@mail.com");
-        type(By.name("Password"),"Igreck$1122");
-        type(By.name("ConfirmPassword"),"Igreck$1122");
+        type(By.name("FirstName"),"Grenck");
+        type(By.name("LastName"),"Iovan");
+        type(By.name("Email"),"igreck3@mail.com");
+        type(By.name("Password"),"Igreck$1222");
+        type(By.name("ConfirmPassword"),"Igreck$1222");
         click(By.name("register-button"));
+        Assert.assertTrue(isElementPresent(By.cssSelector("[href='/logout']")));
     }
 
 }
