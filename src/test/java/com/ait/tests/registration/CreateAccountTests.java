c829ff480b0fe301eedcecee5e040c0693c51007
IonCraciun19 <icraciu21@gmail.com>
2025-06-30 20:02:37 +0200

    Reusable test

--- src/test/java/com/ait/tests/registration/CreateAccountTests.java
+++ src/test/java/com/ait/tests/registration/CreateAccountTests.java
@@ -11,12 +11,15 @@ public class CreateAccountTests extends TestBase{
     String uniqueId =UUID.randomUUID().toString().substring(0,8);
     String email = "testUser_" + uniqueId + "@example.com";
     String password = "PasswordUser$" + uniqueId + "1234";
+    String firstName = "FirstName" + uniqueId;
+    String lastName = "LastName" + uniqueId;
+
 
     @Test
     public void createAccount(){
         click(By.cssSelector("[href='/register']"));
-        type(By.name("FirstName"),"Grenck");
-        type(By.name("LastName"),"Iovan");
+        type(By.name("FirstName"),firstName);
+        type(By.name("LastName"),lastName);
         type(By.name("Email"),email);
         type(By.name("Password"),password);
         type(By.name("ConfirmPassword"),password);
