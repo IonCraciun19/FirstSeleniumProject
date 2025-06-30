8aa549bfb399036424a62c70d64d7348c1fa1efc
IonCraciun19 <icraciu21@gmail.com>
2025-06-30 19:53:40 +0200

    reusable test

--- src/test/java/com/ait/tests/registration/CreateAccountTests.java
+++ src/test/java/com/ait/tests/registration/CreateAccountTests.java
@@ -4,18 +4,24 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.UUID;
+
 public class CreateAccountTests extends TestBase{
+
+    String uniqueId =UUID.randomUUID().toString().substring(0,8);
+    String email = "testUser_" + uniqueId + "@example.com";
+    String password = "PasswordUser$" + uniqueId + "1234";
 
     @Test
     public void createAccount(){
         click(By.cssSelector("[href='/register']"));
         type(By.name("FirstName"),"Grenck");
         type(By.name("LastName"),"Iovan");
-        type(By.name("Email"),"igreck3@mail.com");
-        type(By.name("Password"),"Igreck$1222");
-        type(By.name("ConfirmPassword"),"Igreck$1222");
+        type(By.name("Email"),email);
+        type(By.name("Password"),password);
+        type(By.name("ConfirmPassword"),password);
         click(By.name("register-button"));
         Assert.assertTrue(isElementPresent(By.cssSelector("[href='/logout']")));
-    }
+   }
 
 }
