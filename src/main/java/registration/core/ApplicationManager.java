8f566979eca2a0c4ac6e579e4c59886e2d1f9e40
IonCraciun19 <icraciu21@gmail.com>
2025-07-09 19:39:43 +0200

    CrossTesting

--- src/main/java/registration/core/ApplicationManager.java
+++ src/main/java/registration/core/ApplicationManager.java
@@ -2,6 +2,8 @@ package registration.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import registration.fw.AddItemToCartHelper;
 import registration.fw.RegisteredUserHelper;
 import registration.fw.UserHelper;
@@ -10,14 +12,28 @@ import java.time.Duration;
 
 public class ApplicationManager {
 
+    String browser;
+
     WebDriver driver;
 
     UserHelper user;
     RegisteredUserHelper registeredUser;
     AddItemToCartHelper addItemToCart;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        driver=new ChromeDriver();
+
+        if (browser.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+        }
+
         driver.get("https://demowebshop.tricentis.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
--- src/test/java/com/ait/tests/registration/core/TestBase.java
+++ src/test/java/com/ait/tests/registration/core/TestBase.java
@@ -1,12 +1,14 @@
 package com.ait.tests.registration.core;
 
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import registration.core.ApplicationManager;
 
 public class TestBase {
 
-    protected static ApplicationManager app=new ApplicationManager();
+    protected static ApplicationManager app=new ApplicationManager
+            (System.getProperty("browser", Browser.CHROME.browserName()));
 
     //@BeforeMethod
     @BeforeSuite
